9dfc74c80c3aa6aa6ddee0bc3681eada31f15a9f
Marc Altmann <marcaltmann@posteo.de>
2024-03-08 13:07:25 +0100

    Add Archive model

--- archive/admin.py
+++ archive/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 
-from .models import Interview
+from .models import Archive, Interview
 
+admin.site.register(Archive)
 admin.site.register(Interview)
--- archive/models.py
+++ archive/models.py
@@ -1,8 +1,18 @@
 from django.db import models
 
+class Archive(models.Model):
+    name = models.CharField(max_length=200)
+    public = models.BooleanField()
+
+    def __str__(self):
+        return self.name
+
+
 class Interview(models.Model):
+    archive = models.ForeignKey(Archive, on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
     pub_date = models.DateTimeField("date published")
+    public = models.BooleanField()
 
     def __str__(self):
         return self.title
