e1cbe157872bbbd0c9af5a4c93f2e8bd5bd4b203
Marc Altmann <marcaltmann@posteo.de>
2024-03-08 21:07:06 +0100

    Add more views

--- archive/views.py
+++ archive/views.py
@@ -1,4 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
+
+from .models import Archive
 
 def index(request):
-    return render(request, "archive/index.html")
+    all_archives = Archive.objects.all
+    context = {
+        "archives": all_archives
+    }
+    return render(request, "archive/index.html", context)
+
+def detail(request, archive_id):
+    archive = get_object_or_404(Archive, pk=archive_id)
+    return render(request, "archive/detail.html", {"archive": archive})
