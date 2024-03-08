ce00f9daacef8ab3fc1e7f2e9420334e6f3a7c7f
Marc Altmann <marcaltmann@posteo.de>
2024-03-08 12:32:36 +0100

    Add Interview model

---
+++ archive/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+class Interview(models.Model):
+    title = models.CharField(max_length=200)
+    pub_date = models.DateTimeField("date published")
+
+    def __str__(self):
+        return self.title
