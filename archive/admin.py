571bda9d944ad5cec9a8e1bf8a15606c7b0724b8
Marc Altmann <marcaltmann@posteo.de>
2024-03-09 16:58:03 +0100

    Connect interviews with people

--- archive/admin.py
+++ archive/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
-from .models import Archive, Interview, Person
+from .models import Archive, Interview, Person, InterviewInvolvement
 
 admin.site.register(Archive)
 admin.site.register(Interview)
 admin.site.register(Person)
+admin.site.register(InterviewInvolvement)
--- archive/models.py
+++ archive/models.py
@@ -2,29 +2,6 @@ from datetime import timedelta
 
 from django.db import models
 
-class Archive(models.Model):
-    name = models.CharField(max_length=200)
-    description = models.TextField(default="")
-    public = models.BooleanField()
-
-    def __str__(self):
-        return self.name
-
-
-class Interview(models.Model):
-    archive = models.ForeignKey(Archive, on_delete=models.CASCADE)
-    title = models.CharField(max_length=200, default="")
-    media_type = models.CharField(max_length=100, default="video/mp4")
-    media_url = models.URLField(default="")
-    poster = models.ImageField(default="", blank=True)
-    pub_date = models.DateTimeField("date published")
-    duration = models.DurationField(default=timedelta(seconds=0))
-    public = models.BooleanField(default=True)
-
-    def __str__(self):
-        return self.title
-
-
 class Person(models.Model):
     MALE = "ML"
     FEMALE = "FM"
@@ -51,3 +28,36 @@ class Person(models.Model):
 
     class Meta:
         verbose_name_plural = "people"
+
+
+class Archive(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.TextField(default="")
+    public = models.BooleanField()
+
+    def __str__(self):
+        return self.name
+
+
+class Interview(models.Model):
+    archive = models.ForeignKey(Archive, on_delete=models.CASCADE)
+    title = models.CharField(max_length=200, default="")
+    media_type = models.CharField(max_length=100, default="video/mp4")
+    media_url = models.URLField(default="")
+    poster = models.ImageField(default="", blank=True)
+    pub_date = models.DateTimeField("date published")
+    duration = models.DurationField(default=timedelta(seconds=0))
+    public = models.BooleanField(default=True)
+    people = models.ManyToManyField(Person, through="InterviewInvolvement")
+
+    def __str__(self):
+        return self.title
+
+
+class InterviewInvolvement(models.Model):
+    person = models.ForeignKey(Person, on_delete=models.CASCADE)
+    interview = models.ForeignKey(Interview, on_delete=models.CASCADE)
+    type = models.CharField(max_length=100)
+
+    def __str__(self):
+        return "{}_{}".format(self.person.__str__(), self.interview.__str__())
