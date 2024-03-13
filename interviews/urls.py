f972adb4db0bfa367e5c8e18478d3dc18ffbbc58
Marc Altmann <marcaltmann@posteo.de>
2024-03-13 22:35:24 +0100

    Add debug toolbar

--- interviews/urls.py
+++ interviews/urls.py
@@ -21,4 +21,5 @@ urlpatterns = [
     path('', include('archive.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
     path('admin/', admin.site.urls),
+    path("__debug__/", include("debug_toolbar.urls")),
 ]
