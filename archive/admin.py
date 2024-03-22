770a2c1e015a505c591d6ed1625e3c139e44eda1
Marc Altmann <marcaltmann@posteo.de>
2024-03-22 22:09:42 +0100

    Work on admin

--- archive/admin.py
+++ archive/admin.py
@@ -1,15 +1,50 @@
 from django.contrib import admin
 
-from .models import (Archive, Interview, Transcript, Person,
+from archive.models import (Archive, Interview, Transcript, Person,
                      InterviewInvolvement, Topic, TopicReference,
                      MetadataKey, CharFieldMetadata)
 
-admin.site.register(Archive)
-admin.site.register(Interview)
-admin.site.register(Transcript)
-admin.site.register(Person)
-admin.site.register(Topic)
-admin.site.register(InterviewInvolvement)
-admin.site.register(TopicReference)
-admin.site.register(MetadataKey)
-admin.site.register(CharFieldMetadata)
+
+@admin.register(Archive)
+class ArchiveAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Interview)
+class InterviewAdmin(admin.ModelAdmin):
+    exclude = ["pub_date"]
+
+
+@admin.register(Transcript)
+class TranscriptAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Person)
+class PersonAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Topic)
+class TopicAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(InterviewInvolvement)
+class InterviewInvolvementAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(TopicReference)
+class TopicReferenceAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(MetadataKey)
+class MetadataKeyAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(CharFieldMetadata)
+class CharFieldMetadata(admin.ModelAdmin):
+    pass
