c08f62093afd88119de67cf84159a77df294fcad
Marc Altmann <marcaltmann@posteo.de>
2024-03-23 08:51:31 +0100

    Improve admin interface

--- archive/admin.py
+++ archive/admin.py
@@ -7,12 +7,14 @@ from archive.models import (Interview, Collection, Transcript,
 
 @admin.register(Interview)
 class InterviewAdmin(admin.ModelAdmin):
+    search_fields = ["title"]
     list_display = ["title", "media_type", "duration", "public"]
     exclude = ["pub_date"]
 
 
 @admin.register(Collection)
 class CollectionAdmin(admin.ModelAdmin):
+    search_fields = ["name", "description"]
     fields = ["name", "description", "interviews"]
     list_display = ["name", "interview_count"]
     filter_horizontal = ["interviews"]
@@ -20,34 +22,40 @@ class CollectionAdmin(admin.ModelAdmin):
 
 @admin.register(Transcript)
 class TranscriptAdmin(admin.ModelAdmin):
-    pass
+    search_fields = ["vtt"]
+    list_display = ["interview", "language"]
 
 
 @admin.register(Person)
 class PersonAdmin(admin.ModelAdmin):
+    search_fields = ["last_name", "first_name"]
     list_display = ["last_name", "first_name", "gender", "date_of_birth"]
 
 
 @admin.register(Topic)
 class TopicAdmin(admin.ModelAdmin):
-    pass
+    search_fields = ["name", "gnd_id"]
+    list_display = ["name", "gnd_id"]
 
 
 @admin.register(InterviewInvolvement)
 class InterviewInvolvementAdmin(admin.ModelAdmin):
-    pass
+    autocomplete_fields = ["person", "interview"]
+    list_display = ["person", "interview", "type"]
 
 
 @admin.register(TopicReference)
 class TopicReferenceAdmin(admin.ModelAdmin):
-    pass
+    autocomplete_fields = ["topic"]
+    list_display = ["topic", "interview", "timecode"]
 
 
 @admin.register(MetadataKey)
 class MetadataKeyAdmin(admin.ModelAdmin):
-    pass
+    search_fields = ["label"]
+    list_display = ["label"]
 
 
 @admin.register(CharFieldMetadata)
 class CharFieldMetadata(admin.ModelAdmin):
-    pass
+    list_display = ["interview", "key", "value"]
