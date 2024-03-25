64a014430025e54f7f063b60daed488dd6af272c
Marc Altmann <marcaltmann@posteo.de>
2024-03-25 20:35:12 +0100

    Refactor tests

--- archive/tests.py
+++ archive/tests.py
@@ -2,24 +2,26 @@ from django.test import TestCase
 
 from .models import Interview
 
+def video():
+    return Interview(media_type="video/mp4")
+
+def audio():
+    return Interview(media_type="audio/mp3")
+
 
 class InterviewModelTests(TestCase):
     def test_is_video_with_video(self):
         """ is_video() returns True for videos. """
-        video = Interview(media_type="video/mp4")
-        self.assertIs(video.is_video(), True)
+        self.assertIs(video().is_video(), True)
 
     def test_is_video_with_other(self):
         """ is_video() returns False for non-videos. """
-        audio = Interview(media_type="audio/mp3")
-        self.assertIs(audio.is_video(), False)
+        self.assertIs(audio().is_video(), False)
 
     def test_is_audio_with_audio(self):
         """ is_audio() returns True for audios. """
-        audio = Interview(media_type="audio/mp3")
-        self.assertIs(audio.is_audio(), True)
+        self.assertIs(audio().is_audio(), True)
 
     def test_is_audio_with_other(self):
         """ is_audio() returns False for non-audios. """
-        video = Interview(media_type="video/mp4")
-        self.assertIs(video.is_audio(), False)
+        self.assertIs(video().is_audio(), False)
