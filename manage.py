8ea5c04c46b6b84a47c574c6c00cc458f897d086
Marc Altmann <marcaltmann@posteo.de>
2024-03-25 21:23:36 +0100

    Try to fix webpack test settings

--- manage.py
+++ manage.py
@@ -6,7 +6,8 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'interviews.settings')
+    settings = 'interviews.test_settings' if 'test' in sys.argv else 'interviews.settings'
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', settings)
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
