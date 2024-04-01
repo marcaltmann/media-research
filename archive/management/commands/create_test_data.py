16abba180ed007b7af555061479f065f139a2fe1
Marc Altmann <marcaltmann@posteo.de>
2024-04-01 05:57:59 +0200

    Use Faker directly

--- archive/management/commands/create_test_data.py
+++ archive/management/commands/create_test_data.py
@@ -4,19 +4,83 @@ import random
 from django.db import transaction
 from django.core.management.base import BaseCommand
 
+from faker import Faker
+
 from archive.models import (
-    Person, Resource, Topic, Location, Collection, MetadataKey
-)
-from archive.factories import (
-    PersonFactory,
-    LocationFactory,
-    ResourceFactory,
-    CollectionFactory
+    Person, Resource, Topic, Location, Collection, MetadataKey,
+    ResourceInvolvement,
 )
 
 NUM_PEOPLE = 1000
 NUM_LOCATIONS = 1000
 NUM_COLLECTIONS = 20
+
+
+def create_people():
+    """Creates fake person records."""
+    fake = Faker()
+    people = []
+    for _ in range(NUM_PEOPLE):
+        person = Person.objects.create(
+            first_name = fake.first_name(),
+            last_name = fake.last_name(),
+            date_of_birth = fake.date_of_birth(),
+        )
+        people.append(person)
+    return people
+
+
+def create_resources(people):
+    """Creates fake resource records."""
+    fake = Faker()
+    resources = []
+    for person in people:
+        first_name = person.first_name
+        last_name = person.last_name
+        resource = Resource.objects.create(
+            title=f"{first_name} {last_name}",
+            anon_title=f"{first_name} {last_name[0]}.",
+            duration=datetime.timedelta(minutes=random.randint(10, 360)),
+            pub_date=fake.date_time(tzinfo=datetime.timezone(
+                datetime.timedelta(hours=2), name="CET"))
+        )
+        role = ResourceInvolvement.objects.create(
+            person=person,
+            resource=resource,
+            type=ResourceInvolvement.INTERVIEWEE,
+        )
+        resources.append(resource)
+    return resources
+
+
+def create_locations():
+    """Creates fake location records."""
+    fake = Faker()
+    locations = []
+    for _ in range(NUM_PEOPLE):
+        latitude, longitude, name, _, _ = fake.location_on_land()
+        location = Location.objects.create(
+            name=name,
+            latitude=latitude,
+            longitude=longitude,
+        )
+        locations.append(location)
+    return locations
+
+
+def create_collections():
+    """Creates fake collection records."""
+    fake = Faker()
+    collections = []
+    for _ in range(NUM_COLLECTIONS):
+        collection = Collection.objects.create(
+            name=fake.catch_phrase(),
+            description=fake.paragraph(nb_sentences=5,
+                                       variable_nb_sentences=True),
+        )
+        collections.append(collection)
+    return collections
+
 
 class Command(BaseCommand):
     help = "Generates test data"
@@ -30,26 +94,7 @@ class Command(BaseCommand):
 
         self.stdout.write("Creating new data...")
 
-        # Create people
-        people = []
-        for _ in range(NUM_PEOPLE):
-            person = PersonFactory()
-            people.append(person)
-
-        resources = []
-        # Create resources
-        for _ in range(NUM_PEOPLE):
-            resource = ResourceFactory()
-            resources.append(resource)
-
-        # Create locations
-        locations = []
-        for _ in range(NUM_LOCATIONS):
-            location = LocationFactory()
-            locations.append(location)
-
-        # Create collections
-        collections = []
-        for _ in range(NUM_COLLECTIONS):
-            collection = CollectionFactory()
-            collections.append(collection)
+        people = create_people()
+        resources = create_resources(people)
+        locations = create_locations()
+        collections = create_collections()
