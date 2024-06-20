6085d276a423a53da663e9949079f281a92a120c
Marc Altmann <marcaltmann@posteo.de>
2024-06-20 23:23:53 +0200

    Add legal notice and contact pages

--- pages/urls.py
+++ pages/urls.py
@@ -6,7 +6,8 @@ app_name = "pages"
 
 urlpatterns = [
     path("", views.welcome, name="welcome"),
-    path("terms/", views.terms, name="terms"),
-    path("privacy/", views.privacy, name="privacy"),
     path("accessibility/", views.accessibility, name="accessibility"),
+    path("contact/", views.contact, name="contact"),
+    path("legal_notice/", views.legal_notice, name="legal_notice"),
+    path("privacy/", views.privacy, name="privacy"),
 ]
--- pages/views.py
+++ pages/views.py
@@ -5,8 +5,8 @@ def welcome(request):
     return render(request, "pages/welcome.html")
 
 
-def terms(request):
-    return render(request, "pages/terms.html")
+def legal_notice(request):
+    return render(request, "pages/legal_notice.html")
 
 
 def privacy(request):
@@ -15,3 +15,7 @@ def privacy(request):
 
 def accessibility(request):
     return render(request, "pages/accessibility.html")
+
+
+def contact(request):
+    return render(request, "pages/contact.html")
