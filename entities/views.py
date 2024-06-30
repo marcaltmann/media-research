b2f34495cc314aca73d80758dacf7b9084c8d6d6
Marc Altmann <marcaltmann@posteo.de>
2024-06-30 20:57:22 +0200

    Add entity filter

--- entities/views.py
+++ entities/views.py
@@ -4,8 +4,17 @@ from .models import Entity
 
 
 def entity_index(request):
-    entities = Entity.objects.all()
-    context = {"entities": entities}
+    type_filter = request.GET.get('type')
+    if type_filter:
+        entities = Entity.objects.filter(type=type_filter)
+    else:
+        entities = Entity.objects.all()
+
+    context = {
+        "entities": entities,
+        "type_filter": type_filter,
+        "type_choices": Entity.TYPE_CHOICES,
+    }
     return render(request, "entities/entity_index.html", context)
 
 
